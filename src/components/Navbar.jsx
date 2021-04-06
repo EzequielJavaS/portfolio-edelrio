e2ef3200b054308341886b833d4dd9ca477d9ad3
EzequielJavaS <68125532+EzequielJavaS@users.noreply.github.com>
2021-04-06 20:22:14 +0200

    Start Navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import { makeStyles } from "@material-ui/core";
 import { AppBar, Toolbar } from '@material-ui/core';
 import logo from '../images/favi152.png';
 
 export const Navbar = () => {
+    const classes = useStyles();
     return (
-        <AppBar>
-            <Toolbar>
-                <img src={logo} alt="Logo"/>
+        <AppBar position="sticky" className={classes.root}>
+            <Toolbar className={classes.toolbar}>
+                <img src={logo} className={classes.logo} alt="Logo"/>
             </Toolbar>
         </AppBar>
     )
 }
+
+const useStyles = makeStyles((theme)=>({
+    root:{
+        backgroundColor: "#fafafa",
+    },
+
+    toolbar:{
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center"
+
+    },
+
+
+    logo:{
+        height: "2.8rem",
+        objectFit: "contain",
+        "&:hover": {
+            cursor: "pointer"
+        }
+    }
+  }))
